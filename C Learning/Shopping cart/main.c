00c65efb3c1ac6895adfd6820a5ae0fefc9a2d65
Jaowad Hossain <jaowad.hossain@keyin.com>
2026-02-13 21:21:25 -0330

    First commit from TrueNAS

---
+++ C Learning/Shopping cart/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+ // shopping card program
+
+ char item[50] = "";
+ float price = 0.0f;
+ int quantity = 0;
+ char currency = 0.0f;
+
+ printf("What item would you like to buy?: ");
+ printf("What is the price for each?: ");
+ printf()
+
+    return 0;
+}
